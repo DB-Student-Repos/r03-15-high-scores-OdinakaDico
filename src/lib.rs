85082a93510e62780009266f48ddc64c27fc58fd
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-29 16:17:46 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores {
+    scores: Vec<u32>,
+}
 
 impl HighScores {
     pub fn new(scores: &[u32]) -> Self {
-       HighScores {
+        HighScores {
             scores: scores.to_vec(),
         }
     }
 
     pub fn scores(&self) -> &[u32] {
-       &self.scores
+        &self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        self.scores.last().copied()
+        self.scores.last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        self.scores.iter().max().copied()
+        self.scores.iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-       let mut sorted_scores = self.scores.clone();
+        let mut sorted_scores = self.scores.clone();
         sorted_scores.sort_unstable_by(|a, b| b.cmp(a));
         sorted_scores.into_iter().take(3).collect()
     }
 }
+
